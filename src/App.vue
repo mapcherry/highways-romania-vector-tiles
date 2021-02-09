dc5e4bdcf927fc9f7c890469d560ac050cf4815d
dorinoltean <dorinoltean@gmail.com>
2021-02-09 13:23:44 +0200

    added legend, opening year and road label overlap fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="map"></div>
+
+  <div id="legenda" class="legend">
+    <div class="element">
+      <span class="box existing"></span> <span class="label"> Deschis </span>
+    </div>
+    <div class="element">
+      <span class="box construction"></span>
+      <span class="label"> în execuție </span>
+    </div>
+    <div class="element">
+      <span class="box proposed"></span>
+      <span class="label"> în pregătire </span>
+    </div>
+    <div class="element">
+      <span class="box opening-year">2021</span>
+      <span class="label"> Dată estimată deschidere</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -36,7 +54,7 @@ export default {
           [31.62654341, 49.2208812526],
         ],
         customAttribution:
-          '<a href="https://mapcherry.io/?ref=copyright" target="_blank">&copy; Mapcherry</a> <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+          '<a href="https://mapcherry.io/?ref=copyright" target="_blank">&copy; Mapcherry</a> <a href="https://openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
       });
       map.addControl(new maplibre.NavigationControl());
       map.addControl(new maplibre.FullscreenControl());
@@ -46,82 +64,150 @@ export default {
           tiles: [
             "https://a.tiles.mapcherry.io/highways-ro/{z}/{x}/{y}.pbf?key=" +
               self.token,
+            "https://b.tiles.mapcherry.io/highways-ro/{z}/{x}/{y}.pbf?key=" +
+              self.token,
+            "https://c.tiles.mapcherry.io/highways-ro/{z}/{x}/{y}.pbf?key=" +
+              self.token,
           ],
           minzoom: 5,
           maxzoom: 14,
         });
 
-        map.addLayer({
-          id: "highways-existing",
-          type: "line",
-          source: "highways-ro",
-          "source-layer": "highways-ro",
-          filter: ["all", ["==", "highway", "motorway"]],
-          layout: {
-            "line-join": "round",
-            "line-cap": "round",
-          },
-          paint: {
-            "line-color": "#02A702",
-            "line-width": 3,
-          },
-        });
-
-        map.addLayer({
-          id: "highways-proposed",
-          type: "line",
-          source: "highways-ro",
-          "source-layer": "highways-ro",
-          filter: ["all", ["==", "highway", "proposed"]],
-          layout: {
-            "line-join": "round",
-            "line-cap": "round",
-          },
-          paint: {
-            "line-color": "#292929",
-            "line-width": 3,
-          },
-        });
-
-        map.addLayer({
-          id: "highways-construction",
-          type: "line",
-          source: "highways-ro",
-          "source-layer": "highways-ro",
-          filter: ["all", ["==", "highway", "construction"]],
-          layout: {
-            "line-join": "round",
-            "line-cap": "round",
-          },
-          paint: {
-            "line-color": "#FE5502",
-            "line-width": 3,
-          },
-        });
-
-        map.addLayer({
-          id: "highway_shield",
-          type: "symbol",
-          source: "highways-ro",
-          "source-layer": "highways-ro",
-          filter: ["has", "ref"],
-
-          layout: {
-            "icon-image": "default_3",
-            "icon-rotation-alignment": "viewport",
-            "symbol-placement": {
-              base: 1,
-              stops: [[5, "line"]],
+        map.addLayer(
+          {
+            id: "highways-existing",
+            type: "line",
+            source: "highways-ro",
+            "source-layer": "highways-ro",
+            filter: ["all", ["in", "highway", "motorway", "motorway_link"]],
+            layout: {
+              "line-join": "round",
+              "line-cap": "round",
             },
-            "symbol-spacing": 500,
-            "text-field": "{ref}",
-            "text-font": ["Roboto Regular"],
-            "text-offset": [0, 0.15],
-            "text-rotation-alignment": "viewport",
-            "text-size": 10,
-            "icon-size": 0.8,
+            paint: {
+              "line-color": "#02A702",
+              "line-width": 3,
+            },
           },
-        });
+          "country_3" // insert before layer `country_3` so highways are painted below labels
+        );
+
+        map.addLayer(
+          {
+            id: "highways-proposed",
+            type: "line",
+            source: "highways-ro",
+            "source-layer": "highways-ro",
+            filter: ["all", ["==", "highway", "proposed"]],
+            layout: {
+              "line-join": "round",
+              "line-cap": "round",
+            },
+            paint: {
+              "line-color": "#292929",
+              "line-width": 3,
+            },
+          },
+          "country_3" // insert before layer `country_3` so highways are painted below labels
+        );
+
+        map.addLayer(
+          {
+            id: "highways-construction",
+            type: "line",
+            source: "highways-ro",
+            "source-layer": "highways-ro",
+            filter: ["all", ["==", "highway", "construction"]],
+            layout: {
+              "line-join": "round",
+              "line-cap": "round",
+            },
+            paint: {
+              "line-color": "#FE5502",
+              "line-width": 3,
+            },
+          },
+          "country_3" // insert before layer `country_3` so highways are painted below labels
+        );
+
+        map.addLayer(
+          {
+            id: "highways-construction-opening-date",
+            type: "symbol",
+            source: "highways-ro",
+            "source-layer": "highways-ro",
+            filter: ["all", ["==", "highway", "construction"]],
+            layout: {
+              "text-field": "{opening_date}",
+              "text-font": ["Roboto Medium"],
+              "text-size": {
+                stops: [
+                  [6, 13],
+                  [15, 10],
+                ],
+              },
+              visibility: "visible",
+              "text-allow-overlap": false,
+              "text-ignore-placement": false,
+              "text-letter-spacing": 0.01,
+              "text-max-width": 4,
+              "text-offset": {
+                stops: [
+                  [6, [1, -0.5]],
+                  [15, [0, -2.5]],
+                ],
+              },
+              "text-optional": false,
+              "text-anchor": "left",
+              "text-keep-upright": false,
+              "text-line-height": 0,
+              "text-padding": {
+                stops: [
+                  [6, 25],
+                  [15, 20],
+                ],
+              },
+              "symbol-avoid-edges": true,
+              "symbol-placement": "point",
+              "symbol-spacing": 100,
+              "text-justify": "auto",
+              "text-pitch-alignment": "viewport",
+              "text-rotation-alignment": "viewport",
+            },
+            paint: {
+              "text-color": "#1D8A7F",
+            },
+          },
+          "country_3" // insert before layer `country_3` so highways are painted below labels
+        );
+
+        map.addLayer(
+          {
+            id: "highway_shield",
+            type: "symbol",
+            source: "highways-ro",
+            "source-layer": "highways-ro",
+            filter: ["has", "ref"],
+
+            layout: {
+              "icon-image": "default_3",
+              "icon-rotation-alignment": "viewport",
+              "symbol-placement": {
+                base: 1,
+                stops: [[5, "line"]],
+              },
+              "symbol-spacing": 500,
+              "text-field": "{ref}",
+              "text-font": ["Roboto Regular"],
+              "text-offset": [0, 0.15],
+              "text-rotation-alignment": "viewport",
+              "text-size": 10,
+              "icon-size": 0.8,
+            },
+          },
+          "country_3" // insert before layer `country_3` so highways are painted below labels
+        );
+
         // remove roadshields from motorways in original style
         map.setFilter("road_shield", [
           "all",
@@ -149,10 +235,51 @@ body,
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #map canvas {
   outline: none;
+}
+
+.legend {
+  background-color: #fff;
+  border-radius: 3px;
+  bottom: 30px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  font: 0.9rem "Helvetica Neue", Arial, Helvetica, sans-serif;
+  padding: 10px;
+  position: absolute;
+  left: 10px;
+  z-index: 1;
+}
+.legend .element {
+  display: flex;
+  align-items: center;
+  height: 1.4rem;
+}
+.legend .label {
+  flex: 1 1 auto;
+  padding-left: 10px;
+  font-weight: bold;
+}
+.legend .box {
+  width: 32px;
+  height: 1.1rem;
+  display: inline-block;
+}
+
+.legend .opening-year {
+  font-size: 1rem;
+  font-weight: bold;
+  color: #1d8a7f;
+}
+.legend .existing {
+  background-color: #02a702;
+}
+.legend .construction {
+  background-color: #fe5502;
+}
+.legend .proposed {
+  background-color: #292929;
 }
 </style>
